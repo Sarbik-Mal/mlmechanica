f1805a7de9f5a4de5544d7596bb9cc72ea225dbc
sarbik.keross <sarbik.mal@keross.com>
2025-12-12 09:51:43 +0530

    Initial commit: Added linear regression models simple, multiple, lasso and ridge

---
+++ mlmechanica/regression/__init__.py
@@ -0,0 +1,12 @@
+from .linear.simple import SimpleLinearRegression
+from .linear.multiple import MultipleLinearRegression
+from .linear.lasso import LassoRegression
+from .linear.ridge import RidgeRegression
+
+__all__ = [
+    'SimpleLinearRegression',
+    'MultipleLinearRegression',
+    'LassoRegression',
+    'RidgeRegression',
+]
+
